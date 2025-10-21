3dca2ff58a44e03b3603edcabe769e668eac71bf
wallie-E <996928169@qq.com>
2025-10-21 16:24:04 +0000

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['hzntwvsnjutr.sealoshzh.site'],
+    allowedHosts: ['bkyupjjmtxdq.sealoshzh.site'],
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
